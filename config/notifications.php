6db24bee4015234f37297ceadbaeed0edc4571c2
Artur Bartczak <artur.bartczak@code4.pl>
2016-02-17 19:55:09 +0100

    New notifications

--- config/notifications.php
+++ config/notifications.php
@@ -1,22 +1,29 @@
 <?php
 return [
 
-    'defaultStore' => 'session',
+    'defaultStore'   => 'session',
     'autoCleanOnGet' => true,
 
     'engine' => Code4\Notifications\Engines\BasicNotifications::class,
 
-    'classes'      => [
-        'error'   => 'error',
-        'notice'  => 'notice',
-        'warning' => 'warning',
-        'success' => 'success'
+    'classes' => [
+        'error'       => 'error',
+        'notice'      => 'notice',
+        'warning'     => 'warning',
+        'success'     => 'success',
+        'permissions' => 'permissions',
+        'form'        => 'error'
     ],
-    'icons' => [
-        'error'   => 'fa-times',
-        'notice'  => 'fa-info',
-        'warning' => 'fa-exclamation',
-        'success' => 'fa-check'
+    'icons'   => [
+        'error'       => 'fa-times',
+        'notice'      => 'fa-info',
+        'warning'     => 'fa-exclamation',
+        'success'     => 'fa-check',
+        'permissions' => 'fa-lock',
+        'form'        => 'fa-pencil-square-o'
     ]
 
 ];
+
+
+
